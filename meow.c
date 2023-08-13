658e47930e8c8ea3cf478a82a3f64e1a31c5f19b
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-08-13 08:41:29 +0000

    Sun, Aug 13, 2023, 11:41 AM +03:00

--- meow.c
+++ meow.c
@@ -1,8 +1,8 @@
-#inclumade <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-    printf("meow\n")
-    printf("meow\n")
-    printf("meow\n")
+    printf("meow\n");
+    printf("meow\n");
+    printf("meow\n");
 }
