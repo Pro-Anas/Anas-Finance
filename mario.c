e8f1904a9311805d9d4b14662674c2839079a2fa
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-08-14 16:09:01 +0000

    Mon, Aug 14, 2023, 7:09 PM +03:00

--- mario.c
+++ mario.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-    const int n = 5;
-    for (int i = 0; i < n ; i++)
+    const int n = 3;
+    for (int i = 0; i < n; i++)
     {
-       for (int j = 0; j < n ; j++)
+       for (int j = 0; j < n; j++)
        {
         printf("#");
        }
