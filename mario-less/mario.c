cd7a8f08b06023098b3001502d87d6f30861faf8
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-08-21 06:29:07 +0000

    Mon, Aug 21, 2023, 9:29 AM +03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int hieght;
+    do
+{
+  hight = get_int ("Enter height here");
+}
+   while (height < 1 || hieght > 8);
 }
