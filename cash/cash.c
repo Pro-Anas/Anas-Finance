4c6a3e1226eeacb8bec56e1f242b35d845d02bac
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-08-21 05:30:01 +0000

    Mon, Aug 21, 2023, 8:30 AM +03:00

--- cash/cash.c
+++ cash/cash.c
@@ -52,7 +52,8 @@ int calculate_quarters(int cents)
     int quarters = 0;
     while (cents >= 25)
     {
-        cents = cents - 25
+        cents = cents - 25;
+        quarters++
     }
     }
     return 0;
