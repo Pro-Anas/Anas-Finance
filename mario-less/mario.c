3351743e59766b081fe618c1327659fed186bf63
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-08-21 06:55:50 +0000

    Mon, Aug 21, 2023, 9:55 AM +03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -12,7 +12,7 @@ int main(void)
 
    for (row =0; row < height; row++)
    {
-    for (column = 0; column < height; column++)
+    for (column = 0; column <= height; column++)
     {
         printf("#");
     }
