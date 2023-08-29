e9b431f4a36702873f1c0618d050e9b8a018af4e
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-08-29 08:47:04 +0000

    Tue, Aug 29, 2023, 11:47 AM +03:00

--- scores.c
+++ scores.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
 
 int scores[3];
- scores[0]= 72;
- scores[1]= 73;
- scores[2]= 33;
+ scores[0]= get_int("score: ");
+ scores[1]= get_int("score: ");
+ scores[2]= get_int("score: ");
 
 printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 
