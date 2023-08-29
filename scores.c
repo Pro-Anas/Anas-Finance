f8188d0c2083a24ec43a87de4b14d40047425c09
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-08-29 07:32:03 +0000

    Tue, Aug 29, 2023, 10:32 AM +03:00

--- scores.c
+++ scores.c
@@ -4,9 +4,9 @@ int main(void)
 {
 
 int scores[3];
-int scores[0]= 72;
-int scores[1]= 73;
-int scores[2]= 33;
+ scores[0]= 72;
+ scores[1]= 73;
+ scores[2]= 33;
 
 printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 
