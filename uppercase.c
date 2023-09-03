28c6a7df6b49cc3a20acd8f3f95f5a2106fd587d
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-03 06:10:44 +0000

    Sun, Sep 3, 2023, 9:10 AM +03:00

--- uppercase.c
+++ uppercase.c
@@ -5,5 +5,16 @@
 int main(void)
 {
     string s = get_string("before: ");
-    printf
+    printf("after: ")
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z');
+        {
+            printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf(")
+        }
+    }
 }
