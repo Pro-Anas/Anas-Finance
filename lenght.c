6f74ea904daa98cad9206905f76ba917773875d6
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-03 05:58:24 +0000

    Sun, Sep 3, 2023, 8:58 AM +03:00

--- lenght.c
+++ lenght.c
@@ -1,14 +1,10 @@
 # include <stdio.h>
 #include  <cs50.h>
+#include <string.h>
 
 int main(void)
 {
-    string name = get_string("what's your name? ");
-
-    int n = 0;\
-    while (name[n] != '\0')
-    (
-        n++;
-    )
-    printf("%i\n", n);
+   string name = get_string("what's your name ?);
+   int n = strlen(name);
+   printf("%i\n", n);
 }
