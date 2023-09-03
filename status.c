22ee68e31fb0d3c2f363e1453364f409d18f3488
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-03 07:07:19 +0000

    Sun, Sep 3, 2023, 10:07 AM +03:00

---
+++ status.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+if (arg != 2)
+{
+    printf("Missing commad-line argument\n");
+}
+else
+}
+printf ("hello, %s\n", argv[1]);
