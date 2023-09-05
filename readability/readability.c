2f9327580e45e9cfc131b513a8b37ff3494dfbe5
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-05 05:39:15 +0000

    Tue, Sep 5, 2023, 8:39 AM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+   string text = get_string("Text: ");
 
+   int letters = 0;
+   int words = 0;
+   int sentences = 0;
+
+   for (int i = < strlen(text); i++)
+{
+    if
+    
+}
 }
