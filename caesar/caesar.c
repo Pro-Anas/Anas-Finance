cb642cc79420445bb38753cd93bad228dfbbdefc
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-12 06:09:19 +0000

    Tue, Sep 12, 2023, 9:09 AM +03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -12,7 +12,9 @@ int main(int argc, string argv[])
       return 1;
    }
     for (int i = 0; i < strlen (argv[1]); i++)
-    [
+    {
         if (! is digit(argv[1][i]))
-    {}
+    {
+        printf("Usage: ./ caesar )
+    }
 }
