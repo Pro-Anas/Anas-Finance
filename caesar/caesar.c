7082dd66ff57598cbd110eb7303d311c6e0986e3
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-12 07:40:13 +0000

    Tue, Sep 12, 2023, 10:40 AM +03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,49 +5,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int main(int argc, string argv[])
 {
-   if (argc != 2)
-   {
-      printf("Usage: ./caesar key");
-      return 1;
-   }
-   
-
-    for (int i = 0; i < strlen(argv[1]); i++)
-    {
-        if (!isdigit(argv[1][i]))
+    if (argc != 2)
     {
         printf("Usage: ./caesar key");
         return 1;
     }
 
-
-    int k = atoi(argv[1]);
-
-    string plaintext = get_string("Plaintext: ");
-    printf("Ciphertext: ");
-
-    for ( int j = 0; j < strlen(plaintext); j++)
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-       if (isupper(plaintext[j]))
-       {
-         printf("%c" , (plaintext[j] - 65 + k) % 26 + 65);
-       }
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key");
+            return 1;
+        }
 
-       else if (islower(plaintext[j]))
-       {
-         printf("%c" , (plaintext[j] - 97 + k) % 26 + 97);
-       }
-       else
-       {
-        printf("%c" , plaintext[j]);
-       }
+        int k = atoi(argv[1]);
+
+        string plaintext = get_string("Plaintext: ");
+        printf("Ciphertext: ");
+
+        for (int j = 0; j < strlen(plaintext); j++)
+        {
+            if (isupper(plaintext[j]))
+            {
+                printf("%c", (plaintext[j] - 65 + k) % 26 + 65);
+            }
+
+            else if (islower(plaintext[j]))
+            {
+                printf("%c", (plaintext[j] - 97 + k) % 26 + 97);
+            }
+            else
+            {
+                printf("%c", plaintext[j]);
+            }
+        }
+
+        printf("\n");
     }
-
-       printf("\n");
-
-    }
-
 }
