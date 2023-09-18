87e0f09c45e752f2af983c3c5797e00f5c3bfecf
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-18 09:08:42 +0000

    Mon, Sep 18, 2023, 12:08 PM +03:00

--- search.c
+++ search.c
@@ -9,7 +9,7 @@ int main(void)
      string s = get_string("String: ");
      for (int i = 0; i < 7; i++)
      {
-          if (strcmp(strings[i],s) == s)
+          if (strcmp(strings[i],s) == 0)
           {
              printf("Found\n");
              return 0;
