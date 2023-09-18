11e51f6424c8e3f9a05be5e006184cb91da56688
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-18 08:53:39 +0000

    Mon, Sep 18, 2023, 11:53 AM +03:00

--- search.c
+++ search.c
@@ -6,7 +6,7 @@ int main(void)
 {
      int numbers[] = {20,500,10,5,100,1,50};
 
-     int n = get int("Numbers: ");
+     int n = get_int("Numbers: ");
      for (int i = 0; i < 7; i++)
      {
           if(numbers[i] == n)
