8cae9055c073f6bc221b9c28661f50db4f6780f0
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-18 08:39:45 +0000

    Mon, Sep 18, 2023, 11:39 AM +03:00

--- search.c
+++ search.c
@@ -6,5 +6,14 @@ int main(void)
 {
      int numbers[] = {20,500,10,5,100,1,50};
 
-     int n =
-}
+     int n = get int("Numbers: ");
+     for (int i = 0; i < 7; i++)
+     {
+          if(numbers[i] == n)
+          {
+             printf("Found\n");
+             return 0;
+          }
+     }
+     printf("Not found\n");
+     return 1;
