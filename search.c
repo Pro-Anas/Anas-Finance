3530e704c72b64db4afa9fb7465f39de323ade16
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-18 09:02:21 +0000

    Mon, Sep 18, 2023, 12:02 PM +03:00

--- search.c
+++ search.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-     int numbers[] = {20,500,10,5,100,1,50};
+     string strings[] = {"",500,10,5,100,1,50};
 
      int n = get_int("Numbers: ");
      for (int i = 0; i < 7; i++)
