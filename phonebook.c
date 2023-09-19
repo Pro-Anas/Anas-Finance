248aa8e3f6efbbb7414417e0385bafccd554dada
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-19 06:34:03 +0000

    Tue, Sep 19, 2023, 9:34 AM +03:00

--- phonebook.c
+++ phonebook.c
@@ -20,14 +20,14 @@ int main(void)
    people[0].number = "966+ 569316898";
 
 
-int main(void)
-{
+int main(void);
+
    string name = get_string ("Name ");
    for (int i = 0; i < 2; i++)
    {
-     if (strcmp(names[i], name) == 0)
+     if (strcmp(people[i].name, name) == 0)
      {
-        printf ("Found %s\n", numbers[i]);
+        printf ("Found %s\n", people[i].number);
         return 0;
      }
 
