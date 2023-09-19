bcbcc0e37b7ce90e589daef4549a9cd76efe453e
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-19 06:27:14 +0000

    Tue, Sep 19, 2023, 9:27 AM +03:00

--- phonebook.c
+++ phonebook.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+   string name;
+   string number;
+}
+person;
+
+int main(void)
+{
+   person people[2];
+
+   people[0]
+
+
 int main(void)
 {
    string names [] = {"Anas", "khaled"};
