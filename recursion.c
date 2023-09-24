481c996e7bb4b16147cdf876f48449e8634344b6
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-09-24 07:00:12 +0000

    Sun, Sep 24, 2023, 10:00 AM +03:00

--- recursion.c
+++ recursion.c
@@ -1,3 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+
+
+int main(void)
+{
+    int height = get_int("Hieght: ");
+    draw(height);
+}
+
+void draw(int n)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+
+    draw(n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
