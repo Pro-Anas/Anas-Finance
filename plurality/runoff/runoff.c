b8f6187114cc01f334e277f7b521ee9484cfcd3e
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-10-01 07:14:48 +0000

    Sun, Oct 1, 2023, 10:14 AM +03:00

--- plurality/runoff/runoff.c
+++ plurality/runoff/runoff.c
@@ -184,7 +184,7 @@ int find_min(void)
     int min_votes = 100;
     for (int i = 0; i < candidate_count; i++)
     {
-        if( candidate[i].eliminated == false)
+        if( candidates[i].eliminated == false)
         {
             if (candidates[i].votes < min_votes)
         {
