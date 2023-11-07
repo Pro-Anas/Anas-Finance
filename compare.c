5380c9e5ce37d51b61169ae80c5428a4ebf09d71
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-07 13:13:49 +0000

    Tue, Nov 7, 2023, 4:13 PM +03:00

--- compare.c
+++ compare.c
@@ -6,16 +6,5 @@ int main(void)
     string s = get_string("s: ");
     string t = get_string("t: ");
 
-    if (strcmp(s, t) == 0)
-    {
-        printf("same\n");
-    }
-    else if ( s > t )
-    {
-        printf("s is greater than t\n");
-    }
-    else
-    {
-        printf("s less than t\n");
-    }
+    printf("%p\n", s);
 }
