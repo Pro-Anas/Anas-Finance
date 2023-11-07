dd4f28a0d16adae5cdae391de74e79b5f7f57401
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-07 11:15:08 +0000

    Tue, Nov 7, 2023, 2:15 PM +03:00

--- compare.c
+++ compare.c
@@ -1,12 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
-
+#include <string.h>
 int main(void)
 {
     string s = get_string("s: ");
     string t = get_string("t: ");
 
-    if (s == t)
+    if (strcmp(s, t) == 0)
     {
         printf("same\n");
     }
