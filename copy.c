bffe8f096556956d534da9680b9a43a61aad78a8
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-09 14:11:00 +0000

    Thu, Nov 9, 2023, 5:11 PM +03:00

--- copy.c
+++ copy.c
@@ -1,5 +1,23 @@
 #include <cs50.h>
 #include <ctype.h>
-#include <st.h>
-#include <cs50.h>
-#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+
+    char *t = malloc(strlen(s) +1);
+
+    for (int i = 0; i < strlen(s) + 1; i++)
+    {
+        t[i] = s[i];
+
+    }
+
+    if (strlen(t) > 0)
+    {
+        t[0] = 
+    }
+}
