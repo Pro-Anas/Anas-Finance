0f0e6dd63307274a435f0a29409044c9cf539417
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-12 08:43:53 +0000

    Sun, Nov 12, 2023, 11:43 AM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -54,6 +54,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE temp[height][width];
+    
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
