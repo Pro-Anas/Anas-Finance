e987f39997b22db514434c75cd08b2a383e5725f
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-12 07:35:46 +0000

    Sun, Nov 12, 2023, 10:35 AM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,18 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i - 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            int average = (int)((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0 + 0.5);
+
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+
+        }
+    }
     return;
 }
 
