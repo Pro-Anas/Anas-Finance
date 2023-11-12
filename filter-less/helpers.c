0c1bbd3263de4492772bc593ac476e605fa7600e
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-12 08:57:55 +0000

    Sun, Nov 12, 2023, 11:57 AM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -5,14 +5,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
-            int average = (int)((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0 + 0.5);
+            int average = (int) ((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0 + 0.5);
 
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtRed = average;
-
         }
     }
     return;
@@ -21,7 +20,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++ )
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
@@ -29,9 +28,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int originalGreen = image[i][j].rgbtGreen;
             int originalBlue = image[i][j].rgbtBlue;
 
-            image[i][j].rgbtRed = fmin(255, (int)(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue + 0.5)); //convert red
-            image[i][j].rgbtGreen = fmin(255, (int)(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue + 0.5)); //convert green
-            image[i][j].rgbtBlue = fmin(255, (int)(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue + 0.5)); //convert blue
+            image[i][j].rgbtRed =
+                fmin(255, (int) (0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue + 0.5)); // convert red
+            image[i][j].rgbtGreen =
+                fmin(255, (int) (0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue + 0.5)); // convert green
+            image[i][j].rgbtBlue =
+                fmin(255, (int) (0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue + 0.5)); // convert blue
         }
     }
 }
@@ -62,45 +64,43 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             temp[i][j] = image[i][j];
         }
     }
-        for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
         {
-            for (int j = 0; j < width; j++)
-            {
-                int totalRed = 0, totalGreen = 0, totalBlue = 0;
-                float counter = 0.00;
+            int totalRed = 0, totalGreen = 0, totalBlue = 0;
+            float counter = 0.00;
 
-                for (int x = -1; x < 2; x++)
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
                 {
-                    for ( int y = -1; y < 2; y++)
+                    int currentX = i + x;
+                    int currentY = j + y;
+
+                    if (currentX < 0 || currentX > (height - 1) || currentY < 0 || currentY > (width - 1))
                     {
-                        int currentX = i + x;
-                        int currentY = j + y;
-
-                        if ( currentX < 0 || currentX > (height - 1) || currentY < 0 || currentY > (width -1))
-                        {
-                            continue;
-                        }
-                        totalRed += image[currentX][currentY].rgbtRed;
-                        totalGreen += image[currentX][currentY].rgbtGreen;
-                        totalBlue += image[currentX][currentY].rgbtBlue;
-
-                        counter++;
+                        continue;
                     }
+                    totalRed += image[currentX][currentY].rgbtRed;
+                    totalGreen += image[currentX][currentY].rgbtGreen;
+                    totalBlue += image[currentX][currentY].rgbtBlue;
+
+                    counter++;
                 }
-
-                temp[i][j].rgbtRed = round(totalRed / counter);
-                temp[i][j].rgbtGreen = round(totalGreen / counter);
-                temp[i][j].rgbtBlue = round(totalBlue / counter);
-
             }
+
+            temp[i][j].rgbtRed = round(totalRed / counter);
+            temp[i][j].rgbtGreen = round(totalGreen / counter);
+            temp[i][j].rgbtBlue = round(totalBlue / counter);
         }
-        for (int i = 0; i < height; i++)
-        {
-            for ( int j = 0; j < width; j++)
-            {
-                image[i][j] = temp[i][j];
-            }
-        }
-        return;
     }
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+    return;
+}
