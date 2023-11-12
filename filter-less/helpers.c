4a8786b6be3049aa0cb9d64c3aaa9d254e64bd2f
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-12 08:06:08 +0000

    Sun, Nov 12, 2023, 11:06 AM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,5 +1,5 @@
 #include "helpers.h"
-
+#include "math.h"
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -43,7 +43,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RDBTRIPLE temp = image[i][j];
+            RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][with - 1 - j]; // swap pixels
             image[i][width - 1 - j] = temp;
         }
@@ -53,5 +53,41 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    RGBTRIPLE temp[height][width];
+    for ( int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp[i][j]; = image[i][j];
+        }
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int totalRed = 0, totalGreen = 0, totalBlue = 0;
+                float counter = 0.00
+
+                for (int x = -1; x < 2; x++)
+                {
+                    for ( int y = -1; y < 2; y++)
+                    {
+                        int currentX = i + x;
+                        int currentY = j + y;
+
+                        if ( currrentX < 0; || currentX > (height - 1) || currentY < 0 || currentY > (width -1))
+                        {
+                            continue
+                        }
+                        totalRed += image[currentX][currentY].rgbtRed;
+                        totalGreen += image[currentX][currentY].rgbtGreen;
+                        totalBlue += image[currentX][currentY].rgbtBlue
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+    }
 }
