8bb1a03d314efea037cdb06ed73e7095544c24f0
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-13 10:51:35 +0000

    Mon, Nov 13, 2023, 1:51 PM +03:00

--- list.c
+++ list.c
@@ -15,18 +15,15 @@ int main(void)
      list[1] = 2;
      list[2] = 3;
 
-     int *tmp = malloc(4 * sizeof(int));
+     int *tmp = realloc(list, 4 * sizeof(int));
      if (tmp == NULL)
      {
         free(list);
         return 1;
      }
+     list = tmp;
 
-     for (int i = 0; i < 3; i++)
-     {
-        tmp[i] = list[i];
-     }
-     tmp[3] = 4;
+     list[3] = 4;
 
      free(list);
 
