c08657cd592e153b080e9d00087bae02675ace93
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-15 14:16:34 +0000

    Wed, Nov 15, 2023, 5:16 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -90,7 +90,7 @@ bool load(const char *dictionary)
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
-unsigned int size(void)
+unsigned int word_count = 0;
 {
     // TODO
     return word_count;
