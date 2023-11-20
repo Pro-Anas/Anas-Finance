e07a7eef2a09733e37bf38ddc879419f4fea89c5
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-20 06:03:14 +0000

    Mon, Nov 20, 2023, 9:03 AM +03:00

--- world-cup/sentimental-mario-less/mario.py
+++ world-cup/sentimental-mario-less/mario.py
@@ -1,1 +1,18 @@
 # TODO
+def get_int (prompt):
+
+    while True:
+        try:
+
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid input, Please input a positve integer,")
+
+            height = get_int("Height: ")
+
+            while height < 1 or height > 8:
+                print ("Height must be between 1 and 8")
+                height = het_int("Heigth: ")
+
+                for in in range(1, height + 1):
+                    print(" " * (height - i) + "#" * i)
