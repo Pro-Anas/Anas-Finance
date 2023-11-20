91dff1d2d7206e0427d8d46dab560cb11eded940
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-20 04:28:59 +0000

    Mon, Nov 20, 2023, 7:28 AM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -65,7 +65,7 @@ def simulate_tournament(teams):
     # TODO
     while len(teams) > 1:
         teams = simulate_round(teams)
-        return teams[0]["teams"]
+        return teams[0]["team"]
 
 
 if __name__ == "__main__":
