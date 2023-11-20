de8697aa42146ec4cc8b2ef195635a5fe790638d
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-20 05:40:22 +0000

    Mon, Nov 20, 2023, 8:40 AM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -30,8 +30,11 @@ def main():
     # TODO: Simulate N tournaments and keep track of win counts
      for i in range(N):
         winner = simulate_tournament(teams)
-        if winner in counts
-            cou1
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
+            
         else:
             counts[winner] = 1
 
