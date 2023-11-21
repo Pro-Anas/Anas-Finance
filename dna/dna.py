37ddafdf8ce38c0a4e3fdc055ff98c07d642c98c
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-21 05:56:24 +0000

    Tue, Nov 21, 2023, 8:56 AM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,8 +10,11 @@ def main():
         sys.exit(1)
 
     # TODO: Read database file into a variable
+    with open(sys.argv[2],"r") as sequence_file:
+        sequence = sequence_file.read()
 
     # TODO: Read DNA sequence file into a variable
+    str_counts 
 
     # TODO: Find longest match of each STR in DNA sequence
 
