4dc3f4b4fcdeef1f9f5c0448c8de87380333b871
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-21 04:36:44 +0000

    Tue, Nov 21, 2023, 7:36 AM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,18 @@
 # TODO
+from Cs50 import get_string
+
+text = get_string("Text: ")
+
+letters = 0
+words = 1
+setences = 0
+
+for i in text:
+    if i.isalpha():
+        letters += 1
+    elif i == " ":
+
+        word +=1
+
+    elif i == "," or i == "!" or i == "?":
+        sentences += 1
