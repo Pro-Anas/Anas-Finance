a7d762d584cddc4f29594342770746accbc606b2
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-21 03:56:10 +0000

    Tue, Nov 21, 2023, 6:56 AM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,30 +1,27 @@
-# TODO
-import from Cs50 get_float
+from cs50 import get_float
 
 while True:
     change_owed = get_float("Change Owed: ")
     if change_owed >= 0:
         break
 
-    change_owed_cents = round(change_owed * 100)
+change_owed_cents = round(change_owed * 100)
+num_coins = 0
 
-    num_coins = 0
+while change_owed_cents > 0:
+    if change_owed_cents >= 25:
+        change_owed_cents -= 25
+        num_coins += 1
+    elif change_owed_cents >= 10:
+        change_owed_cents -= 10
+        num_coins += 1
+    elif change_owed_cents >= 5:
+        change_owed_cents -= 5
+        num_coins += 1
+    else:
+        change_owed_cents -= 1
+        num_coins += 1
 
-    while change_owed_cents > 0:
-        if change_owed_cents >= 25:
-            change_owed_cents -= 25
-            num_coins += 1
+print(num_coins)
 
-        elif change_owed_cents >= 10:
-             change_owed_cents -= 10
-              num_coins += 1
-
-        elif change_owed_cents >= 5:
-            change_owed_cents -= 5
-            num_coins += 1
-        else:
-            change_owed_cents -= 1
-            num_coins += 1
-
-            print(num_coins)
 
