547849bf72fa45e3ee626d807a754a4e873d35a5
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-21 03:24:12 +0000

    Tue, Nov 21, 2023, 6:24 AM +03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -10,12 +10,13 @@ def get_int(prompt):
         except ValueError:
             print("Invalid input. Please input a positive integer.")
 
+
 def main():
     height = get_int("Height: ")
 
     for i in range(1, height + 1):
         print(" " * (height - i) + "#" * i)
 
+
 if __name__ == "__main__":
     main()
-
