596ff5742c627f81610cfe0c0825bd62976359b1
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-21 03:22:59 +0000

    Tue, Nov 21, 2023, 6:22 AM +03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,21 @@
 # TODO
+def get_int(prompt):
+    while True:
+        try:
+            number = int(input(prompt))
+            if 1 <= number <= 8:
+                return number
+            else:
+                print("Height must be between 1 and 8.")
+        except ValueError:
+            print("Invalid input. Please input a positive integer.")
+
+def main():
+    height = get_int("Height: ")
+
+    for i in range(1, height + 1):
+        print(" " * (height - i) + "#" * i)
+
+if __name__ == "__main__":
+    main()
+
