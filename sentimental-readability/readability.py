5cd854341799aabf10e3f24601ca922a2c7c8aff
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-21 05:41:01 +0000

    Tue, Nov 21, 2023, 8:41 AM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,23 +1,28 @@
-# TODO
-text = "Your text goes here"  # Replace this with the actual text
-letters = 0
-words = 1
-sentences = 0
+def calculate_readability(text):
+    letters = 0
+    words = 1
+    sentences = 0
 
-for i in text:
-    if i.isalpha():
-        letters += 1
-    elif i == " ":
-        words += 1
-    elif i in [".", "!", "?"]:
-        sentences += 1
+    for i in text:
+        if i.isalpha():
+            letters += 1
+        elif i == " ":
+            words += 1
+        elif i in [".", "!", "?"]:
+            sentences += 1
 
-index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
+    index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 
-if index < 1:
-    print("Before Grade 1")
-elif index >= 16:
-    print("Grade 16+")
-else:
-    print("Grade", round(index))
+    if index < 1:
+        return "Before Grade 1"
+    elif index >= 16:
+        return "Grade 16+"
+    else:
+        return f"Grade {round(index)}"
+
+# Prompt the user to enter a text
+user_input_text = input("Enter a text to analyze its readability level: ")
+readability_level = calculate_readability(user_input_text)
+print(readability_level)
+
 
