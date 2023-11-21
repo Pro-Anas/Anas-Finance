3a8de57b64b11109ecdf64322f529e8f4bfe1c54
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-21 04:43:50 +0000

    Tue, Nov 21, 2023, 7:43 AM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -8,11 +8,22 @@ words = 1
 setences = 0
 
 for i in text:
-    if i.isalpha():
-        letters += 1
-    elif i == " ":
+      if i.isalpha():
 
+    letters += 1
+        elif i == " ":
         word +=1
 
-    elif i == "," or i == "!" or i == "?":
-        sentences += 1
+        elif i == "," or i == "!" or i == "?":
+     sentences += 1
+
+     index = 0.0588 * (letters/words*100) - 0.296 * (sentences/words*100) - 15.8
+
+        if index < 1:
+            print("Before Grade 1")
+
+        elif index >= 16:
+            print("Grade 16+")
+
+        else
+            print("Grade ", round(index)
