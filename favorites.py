2da7fd86b6353f41218d167b1be7205638e61ac7
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-26 05:59:08 +0000

    Sun, Nov 26, 2023, 8:59 AM +03:00

---
+++ favorites.py
@@ -0,0 +1,12 @@
+1 import csv
+with open ("favorites csv", "r") as file:
+
+   reader = csv.DictReader(file)
+   counts = {}
+for row in reader:
+favorite = row ["language"]
+if favorite in counts:
+counts [favorite] += 1 else:
+counts [favorite] = 1
+13 for favorite in counts:
+print (f" {favorite} :)
