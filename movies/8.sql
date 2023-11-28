5d99f8a7f481a5205176dc159ed2f2805f081d03
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-28 12:02:49 +0000

    Tue, Nov 28, 2023, 3:02 PM +03:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,5 +1,5 @@
-LIst the names of all people wo starred in
+
  SELECT name FROM people
 JOIN stars ON stars.person_id = people.id
-JOIN movies ON movies.id = stars. movie id
+JOIN movies ON movies.id = stars.movie_id
  WHERE movies.title="Toy Story";
--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,5 @@
  SELECT DISTINCT name FROM people
- JOIN stars ON stars.person id = people.id
+ JOIN stars ON stars.person_id = people.id
 JOIN movies ON movies.id = stars.movie_id
 WHERE movies.year = 2004
  ORDER BY people.birth ASC;
