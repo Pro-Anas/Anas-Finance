1ebe55945dd9477dadf7d4f23c39248bca5cf7b4
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-28 08:03:37 +0000

    Tue, Nov 28, 2023, 11:03 AM +03:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,4 +1,4 @@
 
 
-SELECT name FROM songs WHERE artist_id = (SELECT id FROM the artists
- WHERE name = 'Post Malone');
+SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists
+WHERE name = 'Post Malone');
