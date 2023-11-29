808a716be6ded4f5a44df1732fcf1a29b7649188
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-29 04:18:49 +0000

    Wed, Nov 29, 2023, 7:18 AM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -31,14 +31,9 @@ AND atm_transactions.transaction_type = "withdraw";
 
 
 
-SELECT caller, receiver FROM phone_calls
+SELECT caller,caller_name, receiver, receiver_name FROM phone_calls
 WHERE year = 2021
 AND month = 7
 AND day = 28
 AND duration < 60;
 
-ALTER TABLE phone_calls
-ADD caller_name text;
-
-ALTER TABLE phone_calls
-ADD receiver_name text;
