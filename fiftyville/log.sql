57361d66f830e724b4ee4ad27c0b307cda841d86
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-11-29 04:10:52 +0000

    Wed, Nov 29, 2023, 7:10 AM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -26,21 +26,6 @@ AND atm_location = "Leggett Street"
 AND atm_transactions.transaction_type = "withdraw";
 
 
-
-
-
-UPDATE phone_calls
-SET caller_name = people.name
-FROM people
-WHERE phone_calls.caller = people.phone_number;
-
-
-UPDATE phone_calls
-SET receiver_name = people.name
-FROM people
-WHERE phone_calls.receiver = people.phone_number;
-
-
 SELECT caller, caller_name, receiver, receiver_name FROM phone_calls
 WHERE year = 2021
 AND month = 7
