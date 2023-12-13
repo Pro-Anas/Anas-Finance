c706cac98eb2c4b0273f783bc21d2e7beeb7c536
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-12-13 07:17:55 +0000

    Wed, Dec 13, 2023, 10:17 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -37,7 +37,8 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
+        rows = db.execute("SELECT * FORM birthdays")
 
-        return render_template("index.html")
+        return render_template("index.html", rows=rows)
 
 
