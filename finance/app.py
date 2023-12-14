a79037354baac8a3de0bea143d575a88ae9879a0
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-12-14 06:22:02 +0000

    Thu, Dec 14, 2023, 9:22 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -64,11 +64,11 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
