ef09d4121cc0af5e24f629bc5a2c33461ae813e2
Pro-Anas <141397268+Pro-Anas@users.noreply.github.com>
2023-12-18 13:48:43 +0000

    Mon, Dec 18, 2023, 4:48 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -1,7 +1,6 @@
 import os
-
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -21,7 +20,6 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -30,13 +28,11 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
 @app.route("/")
 @login_required
 def index():
     """Show portfolio of stocks"""
     return apology("TODO")
-
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
@@ -44,64 +40,39 @@ def buy():
     """Buy shares of stock"""
     return apology("TODO")
 
-
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
     return apology("TODO")
 
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
-    # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
-
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-        # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
-        # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-
 
 @app.route("/logout")
 def logout():
     """Log user out"""
-
-    # Forget any user_id
     session.clear()
-
-    # Redirect user to login form
     return redirect("/")
-
 
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
@@ -109,54 +80,40 @@ def quote():
     """Get stock quote."""
     return apology("TODO")
 
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
     session.clear()
 
     if request.method == "POST":
-        # Check for username
         if not request.form.get("username"):
             return apology("Must provide username", 400)
-
-        # Check for password
         elif not request.form.get("password"):
             return apology("Must provide password", 400)
-
-        # Check for password confirmation
         elif not request.form.get("confirmation"):
             return apology("Must confirm password", 400)
-
-        # Check if passwords match
         elif request.form.get("password") != request.form.get("confirmation"):
             return apology("Passwords do not match", 400)
 
-        # Check if username already exists
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
         if len(rows) != 0:
             return apology("Username already exists", 400)
 
-        # Insert new user into the database
         db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
                    request.form.get("username"), generate_password_hash(request.form.get("password")))
 
-        # Query database for the user
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-        # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
         return redirect("/")
 
-    # User reached route via GET
     else:
         return render_template("register.html")
-
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
     """Sell shares of stock"""
     return apology("TODO")
+
+if __name__ == "__main__":
+    app.run(debug=True)  # Set debug=False in a production environment
